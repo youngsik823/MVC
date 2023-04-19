1bf7b619e7e9c3dda4f2ebaaf2b2162308a106fe
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 14:36:05 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap02/ControllerV2.java
+++ src/main/java/com/spring/mvc/chap02/ControllerV2.java
@@ -8,13 +8,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-
 // 응답 데이터 처리
-@RequestMapping("/model/*")
+@RequestMapping("/model")
 @Controller
 public class ControllerV2 {
 
-    // /model/hobbies : GET
+    // /model/hobbies  : GET
     // hobbies.jsp파일로 이름정보와
     // 취미목록 정보를 보내주고 싶을 때
 
@@ -23,22 +22,40 @@ public class ControllerV2 {
     public String hobbies(Model model) {
 
         String name = "멍멍이";
-        List<String> hobbies = List.of("산책", "밥먹기", "낮잠자기");
-        // jsp로 보낼 데이터를 모델에 담는다.
+        List<String> hobbies
+                = List.of("산책", "밥먹기", "낮잠자기");
+
+        // jsp로 보낼 데이터를 모델에 담는다
         model.addAttribute("n", name);
         model.addAttribute("hList", hobbies);
-//        /WEB-INF/views/chap2/hobbies.jsp
-        return "chap2/hobbies";
+
+//        /WEB-INF/views/chap02/hobbies.jsp
+        return "chap02/hobbies";
     }
-    
+
+
     // == 2. ModelAndView 사용하기
-    // /model/hobbies2 : GET
+    // /model/hobbies2  :  GET
     // hobbies.jsp를 포워딩
 
     // ModelAndView는 리턴 타입을 ModelAndView로 설정
     public ModelAndView hobbies2() {
 
+        // jsp로 보내야 할 데이터
+        String name = "짹짹이";
+        List<String> hList
+                = List.of("전깃줄에서 졸기", "조잘대기");
 
+        // 포워딩할 뷰의 이름
+        String viewName = "chap02/hobbies";
+
+        // ModelAndView 객체에 위 데이터 담기
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName(viewName);
+        mv.addObject("n", name);
+        mv.addObject("hList", hList);
+
+        return mv;
     }
 
 }
