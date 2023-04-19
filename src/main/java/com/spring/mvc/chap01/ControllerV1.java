36fad7023ad08b1049f4b5f63946cefc4ba83fec
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 10:30:29 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -1,9 +1,13 @@
 package com.spring.mvc.chap01;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 어떤 요청들을 처리할지 공통 URL을 설계
 @RequestMapping("/spring/*") // spring으로 시작하는것은 내가 받을거야!
+// 이 클래스의 객체를 스프링이 관리하도록 빈을 등록
+@Controller // @Component와 같은 개념
 public class ControllerV1 {
 
 
