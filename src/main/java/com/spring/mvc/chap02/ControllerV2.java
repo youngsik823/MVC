200d51cf2ae68a46fb916cb93da098a3990ee832
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 14:13:31 +0900

    커밋커밋

---
+++ src/main/java/com/spring/mvc/chap02/ControllerV2.java
@@ -0,0 +1,11 @@
+package com.spring.mvc.chap02;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 응답 데이터 처리
+@RequestMapping("/model/*")
+@Controller
+public class ControllerV2 {
+    
+}
