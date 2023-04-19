f50d52f4d4f5b52289d309e4b92011dfd32ba597
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 11:39:50 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -75,4 +75,9 @@ public class ControllerV1 {
     // == 4. URL에 경로로 붙어있는 데이터 읽기
     // ==> /spring/member/hong/107
     //  hong이라는 유저의 107번 게시글을 읽고싶음
+    @RequestMapping("member/{userName}/{bNo}")
+    public String member() {
+
+        return "";
+    }
 }
