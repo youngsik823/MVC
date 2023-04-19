4098b2c518f287165ef44c966f058350116c7ba9
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 11:30:28 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap01/OrderRequestDTO.java
+++ src/main/java/com/spring/mvc/chap01/OrderRequestDTO.java
@@ -4,4 +4,10 @@ package com.spring.mvc.chap01;
 // 클라이언트의 요청데이터를 서버에서 받을 때
 // 서버의 응답데이터를 클라이언트로 보낼 때 사용하는 객체
 public class OrderRequestDTO {
+    
+    // 필드명이 클라이언트가 보낸 쿼리스트링 이름과 같아야 함
+    private String oNum; // 주문번호
+    private String goods; // 상품명
+    private int amount; // 주문수량
+    private int price; // 상품가격
 }
