becaddeef526649ba2fb8f89cedd0ca4dea6bb73
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-19 10:29:02 +0900

    커밋커밋

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -1,4 +1,10 @@
 package com.spring.mvc.chap01;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// 어떤 요청들을 처리할지 공통 URL을 설계
+@RequestMapping("/spring/*") // spring으로 시작하는것은 내가 받을거야!
 public class ControllerV1 {
+
+
 }
