23861b10db7afc7f987b5d23498796c2ecbb15b8
youngsik823 <109953656+youngsik823@users.noreply.github.com>
2023-04-20 10:37:20 +0900

    커밋커밋

--- src/test/java/com/spring/mvc/chap04/repository/ScoreRepositoryImplTest.java
+++ src/test/java/com/spring/mvc/chap04/repository/ScoreRepositoryImplTest.java
@@ -17,11 +17,12 @@ class ScoreRepositoryImplTest {
     // 테스트 시나리오
     // - 단언(Assertion) 기법
     @Test
-    @DisplayName("저장소에서 findAll을 호출하면 그 반환된 리스트에는 성적정보가 3개 있어야 한다.")
+    @DisplayName("저장소에서 findAll을 호출하면 " +
+            "그 반환된 리스트에는 성적정보가 3개 있어야 한다.")
     void findAllTest() {
         // given-when-then 패턴
         // given: 테스트를 위해 주어질 데이터 (ex: parameter)
-        
+
         // when: 테스트 실제 상황
         List<Score> scoreList = repository.findAll();
 
@@ -29,17 +30,18 @@ class ScoreRepositoryImplTest {
         System.out.println(scoreList.size() == 3);
 
         // 나는 스코어리스트의 사이즈가 3인 것이 참이라고 단언한다.
-//        Assertions.assertTrue(scoreList.size() == 3);
+//        assertTrue(scoreList.size() == 3);
         assertEquals(3, scoreList.size());
 
         // 나는 리스트의 첫번째 객체의 이름이 뽀로로라고 단언한다.
         assertEquals("뽀로로", scoreList.get(0).getName());
     }
 
+
     @Test
-    @DisplayName("저장소에서 findByStuNum을 호출하여 "+
+    @DisplayName("저장소에서 findByStuNum을 호출하여 " +
             "학번이 2인 학생을 조회하면 " +
-            "그 학생의 국어점수가 33점이고" +
+            "그 학생의 국어점수가 33점이고 " +
             "이름이 춘식이어야 한다.")
     void findOneTest() {
         // given
@@ -52,9 +54,8 @@ class ScoreRepositoryImplTest {
     }
 
     @Test
-    @DisplayName("저장소에서 findByStuNum을 호출하여 "+
-            "학번이 99인 학생을 조회하면 " +
-            "null이 리턴될 것이다.")
+    @DisplayName("저장소에서 findByStuNum을 호출하여 " +
+            "학번이 99인 학생을 조회하면 null이 리턴될 것이다.")
     void findOneTest2() {
         // given
         int stuNum = 99;
@@ -63,4 +64,5 @@ class ScoreRepositoryImplTest {
         // then
         assertNull(score);
     }
+
 }
